48e86d19219b5154924005ed40edd6e830d66658
skhadiya <sid.khadiyadev@gmail.com>
2025-03-26 13:05:34 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,36 +96,6 @@ module.exports = {
       {
         title: 'Brand Guidelines',
         path: '/guides/brand_guidelines/'
-      },
-      {
-        title: 'Overview',
-        path: '/support/',
-        header: true,
-        pages: [
-          {
-            title: 'Help',
-            path: '/support/'
-          },
-          {
-            title: 'FAQ',
-            path: '/support/FAQ/'
-          },
-          {
-            title: 'How to contribute',
-            path: '/support/contribute/'
-          }
-        ]
-      },
-      {
-        title: 'Community',
-        path: '/support/community/',
-        header: true,
-        pages: [
-          {
-            title: 'Information',
-            path: '/support/community/'
-          }
-        ]
       }
     ]
   },
